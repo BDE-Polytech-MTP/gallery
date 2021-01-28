b4e15d82f36816b6b6111f9348dd21faedb471f6
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2021-01-28 21:44:01 +0100

    Display images found in photos/albums directory

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const fastify = require('fastify')({
+    logger: true
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    fastify.register(require('fastify-cors'))
+}
+
+const fastifyStatic = require('fastify-static')
+fastify.register(fastifyStatic, {
+    root: [path.join(__dirname, 'build'), path.join(__dirname, 'photos')],
+    list: true,
+    prefixAvoidTrailingSlash: true
+})
+
+fastify.listen(5000, (err, address) => {
+    if (err) throw err
+    fastify.log.info(`Server listening on ${address}`)
+})
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import 'react-image-lightbox/style.css';
+import Lightbox from 'react-image-lightbox';
+
+const baseURL = 'http://localhost:5000';
+
+function fetchAlbumsNames() {
+  return fetch(`${baseURL}/albums`).then(res => res.json()).then(json => json.dirs)
+}
+
+function fetchAlbumImages(albumName) {
+  return fetch(`${baseURL}/albums/${albumName}`).then(res => res.json()).then(json => json.files)
+}
+
+function AlbumDisplay(props) {
+  const [urls, setUrls] = useState([])
+  const [index, setIndex] = useState(null)
+
+  useEffect(() => {
+    fetchAlbumImages(props.name).then(urls => setUrls(urls.map(url => `${baseURL}/albums/${props.name}/${url}`)))
+  }, [props.name])
+
+  useEffect(() => {
+    if (index === null && urls.length) {
+      setIndex(0)
+    }
+  }, [urls, index])
+
+  return urls.length ? <Lightbox
+              mainSrc={urls[index]}
+              nextSrc={urls[(index + 1) % urls.length]}
+              prevSrc={urls[(index - 1 + urls.length) % urls.length]}
+              onMoveNextRequest={() => setIndex((index + 1) % urls.length)}
+              onMovePrevRequest={() => setIndex((index - 1 + urls.length) % urls.length)}
+              onCloseRequest={() => props.onClose()}
+      /> : null
+}
+
+function Album(props) {
+
+  return <div class="Album">
+    <h1>Album: {props.name}</h1>
+    <button onClick={() => props.onSelect()}>Consulter les photos</button>
+  </div>
+}
 
 function App() {
+
+  const [albums, setAlbums] = useState([])
+  const [shownAlbum, setShownAlbum] = useState(null)
+
+  useEffect(() => {
+    fetchAlbumsNames().then(albums => setAlbums(albums))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      {albums.map(album => <Album name={album} onSelect={() => setShownAlbum(album)} />)}
+      {shownAlbum ? <AlbumDisplay name={shownAlbum} onClose={() => setShownAlbum(null)} /> : null}
     </div>
   );
 }
